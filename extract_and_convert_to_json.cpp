d30e34f07cf002ba6eb139b8c685cfdb8867366b
Yuki Koyama <yuki@koyama.xyz>
2018-07-11 20:13:05 +0900

    Implement "import xml" part

--- extract_and_convert_to_json.cpp
+++ extract_and_convert_to_json.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <iostream>
+#include <string>
+#include <tinyxml2.h>
+
+int main(int argc, char** argv)
 {
-	return 0;
+    if (argc != 2)
+    {
+        std::cerr << "Error" << std::endl;
+        exit(1);
+    }
+    
+    const std::string xml_path { argv[1] };
+    
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(xml_path.c_str());
+
+    return 0;
 }
