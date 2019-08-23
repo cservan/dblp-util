8d27590f9a939c52eedf22f7c6f64be1b2468f71
Christophe Servan <christophe.servan@gmail.com>
2019-08-23 15:55:13 +0200

    changinf to nlohmann/json && parsing all data

--- extract_and_convert_to_json.cpp
+++ extract_and_convert_to_json.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <set>
 #include <tinyxml2.h>
-#include <json11.hpp>
+#include <nlohmann/json.hpp>
 
 int main(int argc, char** argv)
 {
@@ -24,37 +24,58 @@ int main(int argc, char** argv)
     doc.LoadFile(xml_path.c_str());
     
     // Instantiate a JSON object
-    std::vector<json11::Json> list;
-    
-    for (tinyxml2::XMLElement* article_entry = doc.FirstChildElement()->FirstChildElement("article"); article_entry != nullptr; article_entry = article_entry->NextSiblingElement("article"))
+//     std::vector<json11::Json> list;
+    nlohmann::json to_print;
+    std::vector<std::string> entry_types={"article","phdthesis"};
+    std::vector<std::string> entry_types_data={"author","editor","title","booktitle","pages","year","address","journal","volume","number","month","url","ee","cdrom","cite","publisher","note","crossref","isbn","series","school","chapter","publnr"};
+    std::vector<std::string>::iterator iter_entry_types=entry_types.begin();
+    while (iter_entry_types != entry_types.end())
     {
-        // Ignore articles from other than ACM Tranc. Graph.
-        if (article_entry->FirstChildElement("journal") == nullptr) { continue; }
-        if (std::string(article_entry->FirstChildElement("journal")->GetText()) != target_journal_name) { continue; }
-        if (std::string(article_entry->FirstChildElement("volume")->GetText()) != target_volume) { continue; }
-        if (std::string(article_entry->FirstChildElement("number")->GetText()) != target_number) { continue; }
-
-        // Ignore invalid entries
-        if (article_entry->FirstChildElement("author") == nullptr) { continue; }
-        if (article_entry->FirstChildElement("title" ) == nullptr) { continue; }
-        if (article_entry->FirstChildElement("title" )->FirstChild()->ToText() == nullptr) { continue; }
-        
-        // Convert to Json
-        std::vector<json11::Json> authors;
-        for (const auto* author = article_entry->FirstChildElement("author"); author != nullptr; author = author->NextSiblingElement("author"))
+      std::string l_type=(*iter_entry_types);
+        for (tinyxml2::XMLElement* article_entry = doc.FirstChildElement()->FirstChildElement(l_type.c_str()); article_entry != nullptr; article_entry = article_entry->NextSiblingElement(l_type.c_str()))
         {
-            authors.push_back(author->GetText());
+            if (article_entry->FirstChildElement("author") == nullptr) { continue; }
+            if (article_entry->FirstChildElement("title" ) == nullptr) { continue; }
+            if (article_entry->FirstChildElement("title" )->FirstChild()->ToText() == nullptr) { continue; }
+            
+            // Convert to Json
+            
+            
+            
+            nlohmann::json authors = nlohmann::json::array();
+            nlohmann::json data_json ; //= nlohmann::json==obj;
+            
+            
+            std::vector<std::string>::iterator iter_entry_types_data=entry_types_data.begin();
+            while (iter_entry_types_data != entry_types_data.end())
+            {
+                std::string l_type_data=(*iter_entry_types_data);
+                std::cerr << l_type_data << std::endl;
+                if (article_entry->FirstChildElement(l_type_data.c_str()) == nullptr) { iter_entry_types_data++;continue; }
+                if (l_type_data=="author")
+                {
+                    for (const auto* author = article_entry->FirstChildElement("author"); author != nullptr; author = author->NextSiblingElement("author"))
+                    {
+                        authors.push_back(author->GetText());
+                    }
+                    data_json.push_back(nlohmann::json::object_t::value_type(std::string("authors"), authors));
+                }
+                else
+                {
+                    std::string l_data=article_entry->FirstChildElement(l_type_data.c_str())->GetText();
+                    data_json.push_back(nlohmann::json::object_t::value_type(l_type_data, l_data));
+                }
+                iter_entry_types_data++;
+                
+            }
+            data_json.push_back(nlohmann::json::object_t::value_type("type",l_type));
+            to_print.push_back(data_json);
         }
-        json11::Json json = json11::Json::object
-        {
-            { "authors", authors },
-            { "title"  , article_entry->FirstChildElement("title")->GetText() }
-        };
-        list.push_back(json);
+        iter_entry_types++;
     }
 
     // Dump the JSON object
-    std::cout << json11::Json{ list }.dump() << std::endl;
+    std::cout << to_print.dump() << std::endl;
     
     return 0;
 }
